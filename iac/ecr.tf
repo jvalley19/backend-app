40eea84df3d89da0b866aa65c056affff98f2888
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2024-04-02 09:27:38 -0400

    Update ecr.tf

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -3,7 +3,7 @@ resource aws_ecr_repository "repository" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
   tags = {
     git_commit           = "N/A"
